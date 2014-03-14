b59975e6664796a6a7d541cab0575ef392ccb56a
Toby Retallick <toby.retallick@gmail.com>
2014-03-14 16:37:21 +0000

    Takeaway class raises error if order total is incorrect

--- task_2/lib/takeaway.rb
+++ task_2/lib/takeaway.rb
@@ -1,24 +1,20 @@
-require 'bundler/setup'
-require 'twilio-ruby'
-
-account_sid = 'ACdc37c6022146a7917a7fe9a1f8364e39'
-auth_token = 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
+#require 'bundler/setup'
+#require 'twilio-ruby'
+#
+#account_sid = 'ACdc37c6022146a7917a7fe9a1f8364e39'
+#auth_token = 'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'
 
 class Takeaway
 
-  attr_reader :menu
+  attr_accessor :menu
     
   def initialize
     @menu = {'pizza' => 15, 'hamburger' => 6, 'steak' => 3, 'icecream' => 3}
   end
 
-  def total_order(list, total)
-    calculate_total = list.map { |k, v| @menu[k] * v }.inject { |sum, n| sum + n }
-    calculate_total == total
-  end
-    
-  def send_text
-    true
+  def order_sum_check (order_items, total)
+    check_total = order_items.map { |k, v| @menu[k] * v }.inject { |sum, n| sum + n }
+    check_total != total ? (raise('Total wrong'); false) : true
   end
 
 end
--- task_2/spec/takeaway_spec.rb
+++ task_2/spec/takeaway_spec.rb
@@ -9,15 +9,11 @@ describe 'Takeaway' do
   end
 
   it "checks an order total" do
-    expect(takeaway.total_order({'pizza' => 4, 'steak' => 4}, 72)).to eq(72)
+    expect(takeaway.order_sum_check({'pizza' => 4, 'steak' => 4}, 72)).to eq(true)
   end
 
-  xit "raises an error if the order total is incorrect" do
-    expect(takeaway.total_order({'pizza' => 2, 'steak' => 2}, 30)).to raise_error
-  end
-
-  xit "initiates a text if an order is totalled correctly" do
-    expect(takeaway.send_text).to eq(true)
+  it "raises an error if the total is incorrect" do
+    expect{takeaway.order_sum_check({'pizza' => 2, 'steak' => 2}, 30)}.to raise_error
   end
 
 end
