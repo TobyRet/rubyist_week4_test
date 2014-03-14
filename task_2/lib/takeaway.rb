2d1a2cdc538aeaefa7ea30a61dd19d2cf66ead29
Toby Retallick <toby.retallick@gmail.com>
2014-03-14 12:32:48 +0000

    Corrected takeaway order method

--- task_2/lib/takeaway.rb
+++ task_2/lib/takeaway.rb
@@ -6,10 +6,15 @@ class Takeaway
     @menu = {'pizza' => 15, 'hamburger' => 6, 'steak' => 3, 'icecream' => 3}
   end
 
-  def total_order(items)
-   price = []
-   items.each { |item| price << @menu[item] }
-   price.inject { |sum, n| sum + n }
+  #def total_order(items)
+  # price = []
+  # items.each { |item| price << @menu[item] }
+  # price.inject { |sum, n| sum + n }
+  #end
+
+  def total_order(list, total)
+    sub_total = list.map { |k, v| @menu[k] * v }
+    total == sub_total.inject { |sum, n| sum + n }
   end
     
 end
--- task_2/spec/takeaway_spec.rb
+++ task_2/spec/takeaway_spec.rb
@@ -8,7 +8,11 @@ describe 'Takeaway' do
     expect(takeaway.menu['pizza']).to be(15)
   end
 
-  it "correctly sum total an order" do
+  xit "correctly sum total an order" do
     expect(takeaway.total_order(['pizza', 'steak'])).to eq(18)
+  end
+
+  it "correctly sum total an order" do
+    expect(takeaway.total_order({'pizza' => 2, 'steak' => 2}, 36)).to eq(true)
   end
 end
